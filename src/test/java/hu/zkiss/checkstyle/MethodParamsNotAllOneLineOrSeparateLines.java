67d2e862a3b4fdd9a9ee7fcfe3a565e48f032823
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2021-01-28 23:43:19 +0000

    Separate checked files from actual code

--- src/test/java/hu/zkiss/checkstyle/MethodParamsNotAllOneLineOrSeparateLines.java
+++
@@ -1,7 +0,0 @@
-package hu.zkiss.checkstyle;
-
-public class MethodParamsNotAllOneLineOrSeparateLines {
-    public void f1(int p1, int p2, int p3,
-                   int p4,
-                   Object... p5) {}
-}
