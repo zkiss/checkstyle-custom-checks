916c7f50178daa322002233017a7ddbd92234df6
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2021-01-29 00:43:35 +0000

    MethodParameterAlignmentCheck: tests

--- src/test/java/hu/zkiss/checkstyle/MethodParameterAlignmentCheckTest.java
+++ src/test/java/hu/zkiss/checkstyle/MethodParameterAlignmentCheckTest.java
@@ -1,7 +1,23 @@
 package hu.zkiss.checkstyle;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class MethodParameterAlignmentCheckTest {
 
+    private final TestCheckstyle checkstyle = new TestCheckstyle(MethodParameterAlignmentCheck.class);
+
+    @Test
+    void noMethodIssues() {
+        checkstyle.check("alignment/NoMethodIssues.java");
+
+        checkstyle.assertNoViolations();
+    }
+
+    @Test
+    void badMethodParamAlignments() {
+        checkstyle.check("alignment/BadMethodParamAlignments.java");
+
+        checkstyle.assertViolation(4, 5, "Lines in method parameter list declaration must be aligned.");
+        checkstyle.assertViolation(8, 5, "Lines in method parameter list declaration must be aligned.");
+    }
 }
